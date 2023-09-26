618b9d3c2cd9ed3be1907436bd499b95bede9dbc
ydydydydydy <ekdud6887@gmail.com>
2023-09-26 10:58:37 +0900

    studyed

---
+++ workspace.edu/SpringMVC06/src/main/java/kr/spring/config/SecurityConfig.java
@@ -0,0 +1,56 @@
+package kr.spring.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+@Configuration // WebConfig.java에서 SecurityConfig를 읽어오기위한 어노테이션
+@EnableWebSecurity // web에서 Security를 쓰겠다
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	// Spring Security 환경설정하는 클래스
+	// WebSecurityConfigurerAdapter
+	// 요청에 대한 보안 설정을 해주는 클래스
+	
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// 요청에 대한 보안 설정하는 곳
+		CharacterEncodingFilter filter = new CharacterEncodingFilter();
+		filter.setEncoding("UTF-8");
+		filter.setForceEncoding(true);
+		http.addFilterBefore(filter, CsrfFilter.class);
+		
+		// 클라이언트가 요청을 했을 때 권한 설정
+		// 회원인증부분
+		http
+			.authorizeRequests()  /* 요청에 따른 권한을 처리하겠다 */
+				.antMatchers("/")  /* "/"(경로)로 왔을 때 권한처리를 할 것인지 */
+					.permitAll()  /* 누구나 접근가능하게끔 하는 전체 권한 */
+						.and()  /* 권한을 또 추가하겠다 */
+					.formLogin() /* 로그인 보안기능 추가 */
+						.loginPage("/loginForm.do")  /* Spring Security에서 제공하는 로그인 폼이 아닌 우리가 만든 로그인폼을 사용 */
+						.loginProcessingUrl("/login.do")  /* 해당 url로 요청이 들어왔을 때 Spring Security의 자체 로그인 기능 수행 */
+						.permitAll()  /* 누구나 로그인은 사용해야하기때문에 권한은 모두 준다 */
+						.and()  /* 권한 추가 */
+					.logout()  /* 로그아웃 기능 */
+						.invalidateHttpSession(true)  /* Spring Security가 알아서 세션을 만료시키게 한다 */
+						.logoutSuccessUrl("/")  /* 로그아웃 성공 이후 이동할 url을 작성 */
+						.and()
+					.exceptionHandling().accessDeniedPage("/access-denied");  // access-denied.jsp
+					/* 로그인을 하지 않고 특정 페이지에 접근하려고 할 때 특정 url로 요청하겠다 */
+		
+		
+	}
+	
+	@Bean  // 패스워드 인코딩 기능을 메모리에 올리기
+	public PasswordEncoder passwordEncoder() {
+		// 비밀번호 암호화 메소드
+		return new BCryptPasswordEncoder();
+	}
+}
---
+++ workspace.edu/SpringMVC06/src/main/java/kr/spring/entity/MemberUser.java
@@ -0,0 +1,40 @@
+package kr.spring.entity;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import lombok.Data;
+
+@Data
+
+public class MemberUser extends User{
+	// Spring Security에 Member 객체를 담을 수 있게 해주는 클래스
+	
+	private Member member;
+	
+	public MemberUser(String username, String password, Collection<? extends GrantedAuthority> authorities) {
+		// MemberUser 객체 생성시 아이디, 비밀번호, 권한을 입력받는다
+		// 실제로 우리는 이 생성자를 사용하지 않는다
+		// 그럼에도 구현한 이유는? 추상메소드라서 어쩔 수 없이 구현해야 한다
+		super(username, password, authorities);  // 부모에 있는 메서드
+	}
+	// 실제로 우리가 사용할 생성자
+	public MemberUser(Member mvo) {
+		// User 클래스의 생성자를 사용해서 구현한다
+		// 생성자 (아이디, 비밀번호, 권한을 넣어준다)
+		super(mvo.getMemID(), mvo.getMemPassword(),
+				/* User클래스의 생성자에 사용하는 권한정보는 */
+				/* Collection<SimpleGrantedAuthority>로 변경해서 넣어야 함 */
+				mvo.getAuthList().stream()  /* 바이트로 변경 */
+				.map(auth -> new SimpleGrantedAuthority(auth.getAuth()))
+				/* List<Auth> -> Collection 안에 들어갈 수 있게 변경 */
+				.collect(Collectors.toList())
+				/* 최종 컬렉션 리스트로 변경 */
+				);
+		this.member = member;
+	}
+}
