e90abbb3afe0a49612adf3cb2356bd9d9216d556
ydydydydydy <ekdud6887@gmail.com>
2023-10-16 12:33:18 +0900

    studyed

---
+++ workspace.edu/SpringMVC10/src/main/java/kr/spring/entity/Board.java
@@ -0,0 +1,36 @@
+package kr.spring.entity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Entity // Board VO가 Database Table로 만들 때 설정
+@Data 
+public class Board { // VO <--- ORM ---> TABLE
+	
+	@Id // PK 의미   // 1씩 증가하면서 넣기 = auto_increment
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idx; // 게시글 고유번호 (호환을 long형으로 해준다)
+	
+	private String title;
+	
+	@Column(length = 2000) // 길이지정 -> 따로 길이지정을 하지 않으면 255
+	private String content;
+	
+	@Column(updatable = false) // 수정할 때 작성자는 바꾸지 X
+	private String writer;
+	
+	@Column(insertable = false, updatable = false, columnDefinition="datetime default now()")
+	private Date indate;
+	
+	@Column(insertable = false, updatable = false, columnDefinition="int default 0")
+	private Long count;
+	
+
+}
